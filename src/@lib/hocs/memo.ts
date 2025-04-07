fb66931b84d254f73ceca5171d4abd8ec31e93c7
SoominYim <soo15soo@kakao.com>
2025-04-07 16:45:15 +0900

    feat: 기본 과제 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -2,6 +2,16 @@
 import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
 
+/**
+ * 1. 이전 props를 저장할 ref 생성
+ * 2. 메모이제이션된 컴포넌트 생성
+ * 3. equals 함수를 사용하여 props 비교
+ * 4. props가 변경된 경우에만 새로운 렌더링 수행
+ * @param Component 메모이제이션할 컴포넌트
+ * @param _equals 비교 함수
+ * @returns 메모이제이션된 컴포넌트
+ */
+
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
